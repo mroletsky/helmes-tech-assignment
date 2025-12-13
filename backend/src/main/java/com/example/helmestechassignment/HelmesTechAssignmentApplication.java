a8f0a52a88dda8b022bc6d65495c266eccbf471b
mroletsky <roletsky@ut.ee>
2025-12-13 18:31:08 +0200

    Initial commit

---
+++ backend/src/main/java/com/example/helmestechassignment/HelmesTechAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.helmestechassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelmesTechAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelmesTechAssignmentApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/helmestechassignment/HelmesTechAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helmestechassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelmesTechAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
