46f7495703d9f0db227670e408aad282a2546c66
mroletsky <roletsky@ut.ee>
2025-12-14 10:57:15 +0200

    Replaced wildcard imports with single class imports

--- backend/src/main/java/com/example/helmestechassignment/model/UserSectorSelection.java
+++ backend/src/main/java/com/example/helmestechassignment/model/UserSectorSelection.java
@@ -1,5 +1,10 @@
 package com.example.helmestechassignment.model;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
--- backend/src/main/java/com/example/helmestechassignment/repo/SectorRepository.java
+++ backend/src/main/java/com/example/helmestechassignment/repo/SectorRepository.java
@@ -1,7 +1,8 @@
 package com.example.helmestechassignment.repo;
 
 import com.example.helmestechassignment.model.Sector;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
